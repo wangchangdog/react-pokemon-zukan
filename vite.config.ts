700e24541c4f17a57be075b4722741cdde55b2e4
wangchangdog <61542937+wangchangdog@users.noreply.github.com>
2024-10-11 22:38:53 +0900

    Update: base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: command !== 'serve' ? '/react-pokemon-zukan/' : '/',
-  }
-
-  return config
-})
+export default defineConfig(({ command }) => ({
+  plugins: [react()],
+  base: command === 'serve' ? '/' : '/react-pokemon-zukan/',
+}));
